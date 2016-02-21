7c386ffbc46db97595df0dfd065b81bacca2729b
rahmani.sahar <rahmani.sahar@gmail.com>
2016-02-21 16:28:09 -0500

    Final maps, for draft one

--- params/som_sr_short.m
+++ params/som_sr_short.m
@@ -7,6 +7,7 @@ clc
 % Map size (n_1 x n_2) of the Network parametrs 
 %dir: results' file directory 
 
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%  Load all necessarily data
@@ -52,14 +53,14 @@ nums=sz(2); % #of regions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %n_cen: Ordering phase steps
 %n_nei: Tuning phase neighborhood distance, default = 1.
-n_cen=n_1*n_2*500;
+n_cen=1000;
 %>>>> MATLAB NETWORK
- net = newsom(annt,[n_2,n_1],'hextop','linkdist',0.9,n_cen,0.02,1);
+ net = newsom(annt,[n_2,n_1],'hextop','linkdist',0.9,n_cen,0.02,3);
  net.trainParam.epochs = n_cen*3;
  net.trainParam.showWindow =false;
  net.trainParam.showCommandLine=true;
  net.trainParam.show=1000;
- [net,tr] = train(net,annt);
+ net = train(net,annt);
  sim_t=sim(net,annt);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -116,12 +117,7 @@ end
   Mtx_TAB_1=Mtx_TAB_1';
 
   
-  
-for h1=n_1:-1:1
-    for   h2=1:1:n_2
-        
-    end
-end
+ 
 %>>>>end
 
 figure(1)
@@ -131,108 +127,8 @@ figure(2)
     plotsomhits(net,annt) %MATLAB som built-in SOM plots; shows density of each neurans
 figure(3)
    plotsompos(net,annt)
-% figure(3)  
-%     for h1=n_1:-1:1
-%         m1=0;
-%             for h2=1:1:n_2
-%                 m1=m1+1;
-%                 check_s=CAT_1{h1,h2};
-%                 size_ch=size(check_s);
-%                 if (size_ch(2) > 0) 
-%                     params=CAT_1{h1,h2};
-%                     s=scatter(params(42,:),params(24,:)./params(25,:));
-%                     s.MarkerFaceColor = [0 h1/20 h2/20];
-%                     hold on
-%                     ylabel('F(FUV)/F(NUV)')
-%                     xlabel('RHI')
-%                 end
-%             end
-%     end
-%>>>> test plots
-
-% count=2;
-% for i=1:17
-%     i_name=int2str(i);
-%     m1=0;
-%     count=count+2;
-%     figure(count)
-%     for ind=18:33
-%         m1=m1+1;
-%         subplot(4,4,m1)
-%         for h1=n_1:-1:1
-%             for h2=1:1:n_2
-%                 check_s=CAT_1{h1,h2};
-%                 size_ch=size(check_s);
-%                 if (size_ch(2) > 0) 
-%                     params=CAT_1{h1,h2};
-%                     s=scatter(params(ind,:),params(i,:));
-%                     s.MarkerFaceColor = [0 h1/20 h2/20];
-%                     if (size_ch(2) > 2) 
-%                         corr_mat(h1,h2)=corr(params(i,:)',params(ind,:)');
-%                         if (abs(corr_mat(h1,h2)) > 0.5)
-%                             strmax = ['p=',num2str(corr_mat(h1,h2))];
-%                              text(mean(params(ind,:)),mean(params(i,:)),strmax,'Color',[h1*h2/4000 h1*5/2000 h2*4/2000]);
-%                         end
-%                     end
-%                     hold on
-%                 end
-%             end
-%         end
-%     end
-%     name1 = strcat(dir,i_name,'vs_raws_18_to_33_for_',n1st,'by',n2st,'.jpeg');
-%     saveas(figure(count),name1,'jpeg')    
-%     m1=0;
-%     figure(count+1)
-%     for ind=34:39
-%         m1=m1+1;
-%         subplot(2,3,m1)
-%         for h1=n_1:-1:1
-%             for h2=1:1:n_2
-%                 check_s=CAT_1{h1,h2};
-%                 size_ch=size(check_s);
-%                 if (size_ch(2) > 0) 
-%                     params=CAT_1{h1,h2};
-%                     s=scatter(params(ind,:),params(i,:));
-%                     s.MarkerFaceColor = [0 h1/20 h2/20];
-%                     if (size_ch(2) > 2) 
-%                         corr_mat(h1,h2)=corr(params(i,:)',params(ind,:)');
-%                         if (abs(corr_mat(h1,h2)) > 0.5)
-%                             strmax = ['p=',num2str(corr_mat(h1,h2))];
-%                             text(mean(params(ind,:)),mean(params(i,:)),strmax,'Color',[h1*h2/4000 h1*5/2000 h2*4/2000]);
-%                         end
-%                     end
-%                     hold on
-%                 end
-%             end
-%         end
-%     end
-%     name2 = strcat(dir,i_name,'vs_raws_34_to_43_for_',n1st,'by',n2st,'.jpeg');
-%     saveas(figure(count+1),name2,'jpeg')
-% end
-% 
-% for i=1:17
-%     for ind=18:40
-%     i_name=int2str(i);
-%     j_name=int2str(ind);
-%     corr_mat=zeros(n_1,n_2)+0.000000006;
-%         for h1=n_1:-1:1
-%             for h2=1:1:n_2
-%                 check_s=CAT_1{h1,h2};
-%                 size_ch=size(check_s);
-%                 if (size_ch(2) > 2) 
-%                     params=CAT_1{h1,h2};
-%                     corr_mat(h1,h2)=corr(params(i,:)',params(ind,:)');
-%                 end
-%             end
-%         end
-%         II= find(abs(corr_mat) >=0.5) ;
-%         if (II > 0)
-%           name1 = strcat(dir,i_name,'_',j_name,'_',n1st,'_by',n2st,'.csv');   
-%           % table1 = cell2table(corr_mat);
-%          csvwrite(name1,corr_mat);
-%         end
-%     end
-% end
+figure(4)
+    plotsomnd_s(net,annt)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -253,5 +149,5 @@ csvwrite(pos,Mtx_TAB_1);
 saveas(figure(1),fig1,'jpeg')
 saveas(figure(2),fig2,'jpeg')
 saveas(figure(3),fig3,'jpeg')
-close all
+%close all
 end
